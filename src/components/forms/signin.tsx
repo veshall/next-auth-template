eff6b5253744347b630db0d7177711ce7d01055a
veshall <vishalpaliwal51@gmail.com>
2024-06-13 23:44:25 +0530

    add signin form

--- src/components/forms/signin.tsx
+++ src/components/forms/signin.tsx
@@ -1,5 +1,48 @@
 import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
-export default function Signin() {
-  return <form>hello world!</form>;
+const formSchema = z.object({
+  email: z.string().email("Please type a valid email"),
+  password: z.string().min(1, { message: "Please type your password!" }),
+});
+
+const form = useForm({
+  resolver: zodResolver(formSchema),
+});
+
+export default function SigninForm() {
+  return (
+    <Form {...form}>
+      <form className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
